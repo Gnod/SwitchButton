0b55c38b4b9f0f5a41245b438cb5630b5490fd44
Gnod <gnodsy@gmail.com>
2013-09-21 17:26:18 +0800

    Add second example of Using the SwitchButton

--- src/com/example/switchbutton/SwitchActivity.java
+++ src/com/example/switchbutton/SwitchActivity.java
@@ -1,6 +1,9 @@
 package com.example.switchbutton;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.Toast;
@@ -23,6 +26,38 @@ public class SwitchActivity extends Activity {
 				return false;
 			}
 		});
+		
+		SwitchButton switchBtnB = (SwitchButton)findViewById(R.id.btn_switch_b);
+		switchBtnB.setSwitch(true, 0);
+		switchBtnB.setOnSwitchListener(new OnSwitchListener() {
+			
+			@Override
+			public boolean onSwitch(final SwitchButton v, boolean isRight) {
+				if(isRight) {
+					AlertDialog.Builder builder = new AlertDialog.Builder(SwitchActivity.this);
+					builder.setMessage("Confirm to Switch Right?");
+					builder.setPositiveButton("Confirm", new OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							v.setSwitch(true);
+							
+						}
+					});
+					builder.setNegativeButton("Cancel", new OnClickListener() {
+						
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							dialog.dismiss();
+						}
+					});
+					builder.show();
+				}else {
+					v.setSwitch(isRight);
+				}
+				return true;
+			}
+		});
+		
 	}
 
 	@Override
